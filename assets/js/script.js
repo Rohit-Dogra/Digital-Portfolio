83bb007c7d99da0ad18852978b15f7ccf1f88533
Rohit <dogra.rohit2002@gmail.com>
2026-03-25 16:27:56 +0530

    changes done

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "Wordpress", "web development"],
+    strings: [" Full Stack Development", "Frontend Development", "Backend Development", "Web Designing", "Wordpress", "Web Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
